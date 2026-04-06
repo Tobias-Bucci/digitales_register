64aa890708bd0666a043969687c51222abbc8736
Tobi <stbuctob@bx.fallmerayer.it>
2026-04-07 00:16:01 +0200

    Enable edge-to-edge display in MainActivity on creation

--- android/app/src/main/kotlin/it/bucci/digitalesregister/MainActivity.kt
+++ android/app/src/main/kotlin/it/bucci/digitalesregister/MainActivity.kt
@@ -1,12 +1,19 @@
 package it.bucci.digitalesregister
 
 import android.content.Context
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.core.app.NotificationManagerCompat
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+    }
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(
